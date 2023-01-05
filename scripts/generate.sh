9d27875f8a30e52879783ea533e7014c4a1bd97c
Gimmy <gheorghe.popescu@aserto.com>
2023-01-05 15:22:11 +0200

    remove openapi.json after generate

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,4 +5,7 @@ curl \
   -u "${USERNAME}:${READ_WRITE_TOKEN}" \
   -H "Accept: application/vnd.github.v3.raw" \
   https://raw.githubusercontent.com/aserto-dev/openapi-console-backend/${OPENAPI_SHA}/publish/console-backend/openapi.json
+
 yarn openapi-typescript openapi.json --output ./generated/console-backend.ts
+
+rm -rf openapi.json
