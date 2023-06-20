f6bfc288c1a15ed899bd16b6db2946e5f9e2aa5e
Gimmy <gheorghe.popescu@aserto.com>
2023-06-20 10:31:01 +0300

    fix bump

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -2,6 +2,6 @@
 set -e
 
 # bump the samve versio number as the pb repo
-PB_VERSION=$(echo ${PROTO_REF} | sed 's/^v//')
+PB_VERSION=$(echo ${PROTO_REF} | sed 's/^refs\/tags\/v//')
 VERSION=$(npm version ${PB_VERSION} --no-git-tag-version)
 echo $VERSION
